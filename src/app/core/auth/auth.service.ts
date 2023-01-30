c97c9f3b77c1c0596453b0be8b1d195e102d0eef
Matthieu <95905072+MatthieuRt@users.noreply.github.com>
2023-01-30 07:58:02 +0300

    Update auth.service.ts

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -7,7 +7,7 @@ import { UserService } from 'app/core/user/user.service';
 export class AuthService
 {
     _authenticated: boolean = false;
-    baseUrl = 'https://back-garage.vercel.app/';
+    baseUrl = 'back-garage.vercel.app/';
     /**
      * Constructor
      */
